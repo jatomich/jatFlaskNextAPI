5a1a5f60bdcb8c024323795db3cf87a5c266c7a0
jatomich <jatomic@spareparse.com>
2024-02-04 01:07:51 -0600

    Create connection to Azure SQL Server. Addresses issue #20.

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -10,9 +10,9 @@ cors = CORS()
 
 def create_app(config_class=Config):
     app = Flask(__name__,
-                #static_url_path='/',
-                static_url_path='/home/at/SOURCE/jatFlaskNextAPI/flask-server/app/static',
-                static_folder='/app/static'
+                # static_url_path='/flask-server/app/static',
+                # static_url_path='/home/at/SOURCE/jatFlaskNextAPI/flask-server/app/static',
+                # static_folder='/app/static'
     ) 
     app.config.from_object(config_class)
     db.init_app(app)
